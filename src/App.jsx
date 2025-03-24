a4b8c36c9129814ca59793fcee37865c891c14f0
Manel-smll <rachasarasmail@gmail.com>
2025-03-24 15:04:51 +0100

    i add footer to my App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 
 import Header from './components/Header.jsx' 
+import Footer from './components/Footer.jsx';
 function App(){
   return(
     <div>
       <Header/>
+    
       <h1> MyBank</h1>
+      <Footer/>
     </div>
   );
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p> 2025 MyBank | projet realisé par SMAIL Manel</p>
+        </footer>
+    );
+}
